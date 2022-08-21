6b5af6c92b61b243355340dfa471128e19cdb1d2
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-08-21 23:20:02 +0530

    fruit with move issue

---
+++ fruit.rs
@@ -0,0 +1,28 @@
+struct Fruit {
+    apples: i32,
+    bananas: i32,
+}
+
+fn count_fruit(fruit: Fruit){
+    println!(
+        "I've got {} apples and {} bananas",
+        fruit.apples, fruit.bananas
+    );
+}
+
+fn price_fruit(fruit: Fruit) -> i32{
+    fruit.apples * 8 + fruit.bananas * 12
+}
+
+fn main(){
+    let fruit = Fruit{
+        apples: 10,
+        bananas: 5
+    };
+
+    count_fruit(fruit); //owner changed to count_fruit --MOVE
+
+    let price = price_fruit(fruit); //we're using a moved value, something we don't own
+    println!("I can make {} cents", price);
+
+}
