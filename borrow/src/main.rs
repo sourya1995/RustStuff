99c82a25bc3e08d537124cfad0907841c99786de
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-01-26 21:18:41 +0530

    concept: borrowing/lending clear

--- borrow/src/main.rs
+++ borrow/src/main.rs
@@ -11,6 +11,14 @@ fn own_string(s: String){
     println!("{}", s);
 }
 
+fn borrow_vec(vector: &Vec<i32>){
+    println!("{:?}", vector);
+}
+
+fn borrow_string(s: &String) {
+    println!("{}", s);
+}
+
 fn main(){
     let mut my_vector = vec![1, 2, 3, 4, 5];
     let my_integer = 10;
@@ -19,12 +27,17 @@ fn main(){
     own_integer(my_integer); 
     println!("{}", my_integer); //this runs fine
 
-    own_string(my_string);  //also runs fine
+    //own_string(my_string);  //won't run
+    own_string(my_string.clone());
     //println!("{}", my_string); //this does not run because at line 22, my_string is owned by own_string and therefore cannot be owned by two different parts
     //of the code
+    borrow_string(&my_string);
 
-    own_vector(my_vector); //runs fine
+    // own_vector(my_vector); //won't run
+    own_vector(my_vector.clone()); //cloning the object so it can be used
     println!("{:?}", my_vector); //this does not run because at line 26, my_vector is owned by own_vector and therefore cannot be owned by two different parts
     //of the code
+
+    borrow_vec(&my_vector); //I am lending my_vector to borrow_vec which is going to cause line 30 to fail now.
 
 }
