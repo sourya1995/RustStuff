c5936e757cd6cf685f0391aeb3759957e967e18e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-01-27 09:15:40 +0530

    concept: basic error handling

---
+++ FileReader/target/debug/FileReader.d
@@ -0,0 +1,1 @@
+/Users/souryabhattacharya/Desktop/RustStuff/FileReader/target/debug/FileReader: /Users/souryabhattacharya/Desktop/RustStuff/FileReader/src/main.rs
---
+++ FileReader/target/debug/deps/FileReader-e49ee3f8424e663e.d
@@ -0,0 +1,5 @@
+/Users/souryabhattacharya/Desktop/RustStuff/FileReader/target/debug/deps/FileReader-e49ee3f8424e663e: src/main.rs
+
+/Users/souryabhattacharya/Desktop/RustStuff/FileReader/target/debug/deps/FileReader-e49ee3f8424e663e.d: src/main.rs
+
+src/main.rs:
